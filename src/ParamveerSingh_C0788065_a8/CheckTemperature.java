e990fb991632852cdb5dd83a677d688c2f944153
Paramveer <paramveersidhu0@gmail.com>
2021-03-30 18:48:58 -0400

    Welcome

--- src/ParamveerSingh_C0788065_a8/CheckTemperature.java
+++ src/ParamveerSingh_C0788065_a8/CheckTemperature.java
@@ -1,4 +1,11 @@
 package ParamveerSingh_C0788065_a8;
 
+import javax.swing.*;
+
 public class CheckTemperature {
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null, "Welcome to the program.\n" +
+                "Here you can calculate feels like temperature \n" +
+                "based upon the day, temperature and wind speed.");
+    }
 }
