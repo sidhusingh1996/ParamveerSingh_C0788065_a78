589b1dd15a2ce227092d04508ff53976d4c152ae
Paramveer <paramveersidhu0@gmail.com>
2021-03-30 18:38:36 -0400

    Assignment_08 Starting Commit

--- src/ParamveerSingh_C0788065_a8/Temperature.java
+++ src/ParamveerSingh_C0788065_a8/Temperature.java
@@ -1,4 +1,17 @@
 package ParamveerSingh_C0788065_a8;
 
-public class Temperature {
+public class Temperature{
+    private final String day;
+    private final float temperature;
+    private final float wind_speed;
+
+    public Temperature(String day, float temperature, float wind_speed){
+        this.day = day;
+        this.temperature = temperature;
+        this.wind_speed = wind_speed;
+    }
+
+    public float feel_like_temperature(){
+        return temperature * wind_speed;
+    }
 }
